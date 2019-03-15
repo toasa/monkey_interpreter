fa163f8cbb602edb4f42d0dbe704368d3549b0f7
toasa <atsushi.tohyama.160.333@gmail.com>
2019-03-15 15:45:39 +0900

    add: builtin-func len and first of object.Array

--- eval/builtins.go
+++ eval/builtins.go
@@ -12,8 +12,29 @@ var builtins = map[string]*object.Builtin {
             switch arg := args[0].(type) {
             case *object.String:
                 return &object.Integer{Value: int64(len(arg.Value))}
+            case *object.Array:
+                return &object.Integer{Value: int64(len(arg.Elems))}
             default:
                 return newError("argument to `len` not supported, got %s", arg.Type())
+            }
+        },
+    },
+    "first": &object.Builtin {
+        Fn: func(args ...object.Object) object.Object {
+            l := len(args)
+            if l != 1 {
+                return newError("wrong number of arguments. got=%d, want=1", l)
+            }
+
+            switch arg := args[0].(type) {
+            case *object.Array:
+                if len(arg.Elems) == 0 {
+                    return NULL
+                } else {
+                    return arg.Elems[0]
+                }
+            default:
+                return newError("argument to `first` not supported, got %s", arg.Type())
             }
         },
     },
